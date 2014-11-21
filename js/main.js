cde3b6db81d6cf60e8bbb88c437d0ed1fe508542
Todd Redmond <tredmond@urbangalaxie.com>
2014-11-20 22:11:30 -0800

    Finished data for park swap

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-$(document).ready(function(){
-  $("address").each(function(){                         
-    var embed ="<iframe width='425' height='350' frameborder='0' scrolling='no'  marginheight='0' marginwidth='0'   src='https://maps.google.com/maps?&amp;q="+ encodeURIComponent( $(this).text() ) +"&amp;output=embed'></iframe>";
-          $(this).html(embed);
-                             
-   });
-});
 
 var data={"parks":[
         {
@@ -15,12 +8,39 @@ var data={"parks":[
             "Size":"3.75 acres"
         },
         {
+            "ParkName":"The Fields in the Pearl",
+            "pLocation":"NW 11th Ave. and NW Overton St",
+            "ParkType":"Shared",
+            "SmallDog":"No",
+            "Size":"1.41 acres"
+        },
+        {
             "ParkName":"Gabriel Park",
             "pLocation":"SW 45th Ave. and SW Vermont St.",
             "ParkType":"Dedicated",
             "SmallDog":"No",
             "Size":"1.75 acres"
-
+        },
+        {
+            "ParkName":"Alberta Park",
+            "pLocation":"NE 22nd Ave. & Killingsworth St.",
+            "ParkType":"Shared",
+            "SmallDog":"No",
+            "Size":"1.32 acres"
+        },
+        {
+            "ParkName":"Brentwood Park",
+            "pLocation":"SE 60th Ave. & Duke St.",
+            "ParkType":"Dedicated",
+            "SmallDog":"No",
+            "Size":".57 acres"
+        },
+        {
+            "ParkName":"North Clackamas Park",
+            "pLocation":"5440 SE Kellogg Creek Dr.",
+            "ParkType":"Dedicated",
+            "SmallDog":"No",
+            "Size":"1.26 acres"
         }
 ]}
 function changeText(x) {
@@ -29,4 +49,14 @@ function changeText(x) {
    document.getElementById('type').innerHTML = data.parks[x].ParkType;
    document.getElementById('small').innerHTML = data.parks[x].SmallDog;
    document.getElementById('size').innerHTML = data.parks[x].Size;
+   document.getElementById('address').innerHTML = data.parks[x].pLocation;
+//}
+
+//$(document).ready(function(){
+  $("#address").each(function(){                         
+    var embed ="<iframe width='425' height='350' frameborder='0' scrolling='no'  marginheight='0' marginwidth='0'   src='https://maps.google.com/maps?&amp;q="+ encodeURIComponent( $(this).text() ) +"&amp;output=embed'></iframe>";
+          $(this).html(embed);
+                             
+   });
+//});
 }
